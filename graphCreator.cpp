9a3bb8a75cfd8866b5ab9797527007181b5a8983
Proxeria <napinpdx@me.com>
2022-06-07 19:45:37 -0700

    can add vertex

--- graphCreator.cpp
+++ graphCreator.cpp
@@ -1,16 +1,67 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-class Graph {
+class Edge {
 public:
   
 private:
-  //int array(Graph, source); 
+  string dest; //edge's destination
+  int weight;
 };
 
-//psuedocode from https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
-   // function Dijkstra(Graph, source): //name it diAlgo
+class Vertex {
+public:
+  Vertex(std::string newLabel) {
+    label = newLabel;
+  }
+  string getLabel() {
+    return label;
+  }
+private:
+  string label;
+  vector<Edge> edgeVect;
+};
+
+class Graph {
+public:
+  void addVert(string);
+  void addEdge(string);
+  void remVert();
+  void remEdge();
+  void findSP();
+  void print();
+private:
+  void diAlgo(); //Dijkstra's 
+  map<string, Vertex*> vertices;
+};
+
+void Graph::addVert(string vertLabel) {
+  //Vertex* x = new Vertex(vertLabel);
+  vertices[vertLabel] = new Vertex(vertLabel);
+}
+
+void Graph::addEdge(string label) {
+  
+}
+
+void Graph::remVert() {
+  
+}
+
+void Graph::remEdge() {
+  
+}
+
+void Graph::findSP() {
+  
+}
+
+void Graph::diAlgo() {
+  //psuedocode from https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+// function Dijkstra(Graph, source): //name it diAlgo
  
    //     for each vertex v in Graph.Vertices:            
    //         dist[v] ← INFINITY                 
@@ -29,24 +80,40 @@ private:
    //                prev[v] ← u
 
    //    return dist[], prev[]
+}
+
+void Graph::print() {
+  for (const auto& v : vertices) {
+    cout << v.first << ": ";
+    cout << v.second->getLabel();
+    //print edges
+    cout << endl;
+  }
+}
 
 int main() {
   string usrIn;
+  Graph g;
+  
   cout << "Commands (Case Sensitive):" << endl;
   cout << "av (Add Vertex): Enter a label for a vertex. Add it to the graph." << endl;
   cout << "rv (Remove Vertex): Enter a vertex label and remove it from the graph. Remove all edges to that vertex." << endl;
   cout << "ae (Add Edge): Enter two node names and a weight (number). Add an edge between them, from the first node to the second node." << endl;
   cout << "re (Remove Edge): Enter two vertex labels, and remove an edge between them." << endl;
   cout << "fsp (Find Shortest Path): Enter two vertex labels, prints shortest bath between them." << endl;
+  cout << "p (print): Prints adjacency table" << endl;
   cout << "q (Quit): Exits the program." << endl;
 
   while (true) {
     cout << "Command: ";
     cin >> usrIn;
-    cout << endl;
+    //cout << endl;
 
     if (usrIn == "av") {
-      cout << "Add Vertex" << endl;
+      cout << "Enter name of vertex: " << endl;
+      cin >> usrIn;
+      g.addVert(usrIn);
+      cout << "Vertex added" << endl;
     }
     else if (usrIn == "rv") {
       cout << "Remove Vertex" << endl;
@@ -63,6 +130,10 @@ int main() {
     else if (usrIn == "q") {
       cout << "Quit" << endl;
       break;
+    }
+    else if (usrIn == "p") {
+      cout << "print" << endl;
+      g.print();
     }
   }
 }
